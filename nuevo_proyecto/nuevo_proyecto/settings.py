dc6f4e08abc00c3c1b2689bd64d631c6d303fc17
Ruben <ruben.caballero.lopez@gmail.com>
2025-05-07 17:15:28 +0200

    config vs parse

--- nuevo_proyecto/nuevo_proyecto/settings.py
+++ nuevo_proyecto/nuevo_proyecto/settings.py
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'nuevo_proyecto.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
+    'default': dj_database_url.config(
         default= f'sqlite:///{BASE_DIR/"db.sqlite3"}',
         conn_max_age=600,
         ssl_require= os.getenv('DATABASE_URL', '').startswith('postgres')
