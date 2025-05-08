bf04f300ca49ed2e0c1a8ea6c8cbb5b221d198ce
Ruben <ruben.caballero.lopez@gmail.com>
2025-05-08 18:49:03 +0200

    elimino modulo dotenv

--- nuevo_proyecto/nuevo_proyecto/settings.py
+++ nuevo_proyecto/nuevo_proyecto/settings.py
@@ -2,7 +2,6 @@ from pathlib import Path
 from decouple import config
 import os
 import dj_database_url
-from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
